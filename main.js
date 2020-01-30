94a8c5175c344cb7fe6565863d73869c8e535ae8
Anca Simon <ancarsimon@gmail.com>
2020-01-29 21:19:48 -0600

    wip still working on building student object and array and pushign data to a card

--- main.js
+++ main.js
@@ -13,20 +13,44 @@ const buildForm = () => {
         <form class="form-inline centered">
             <label class="my-1 mr-2" for="studentName">Student: </label>
             <input type="text" class="form-control mb-2 mr-sm-2" id="studentName">
-            <button type="submit" class="btn btn-primary mb-2">Sort!</button>
+            <button type="submit" id="add-button" class="btn btn-primary mb-2">Sort!</button>
         </form>
         `
     printToDom('form', formContents);
 };
 
+const houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
 
-const addStudent = () => {
+const randomHouse = houses[Math.floor((Math.random() * houses.length) + 1)];
+console.log(randomHouse);
 
+const buildStudent = () => {
+    const studentInfo = 
+    {
+        name: studentName.value,
+        house: randomHouse
+    };
+    console.log(studentInfo);
+};
+
+const buildArray = () => {
+    const studentArray = [studentInfo];
+    studentInfo = `
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${studentName}</h5>
+                    <p class="card-text">${houses.name}</p>
+                    <a href="#" class="btn btn-primary">Expel</a>
+            </div>
+            `
+
+    studentArray.push(studentInfo);
+    printToDom('studentsList', "studentArray");
 }; 
 
 const events = () => {
     document.getElementById('sorting-button').addEventListener('click', buildForm);
-    // document.getElementById(ID).addEventListener('click', addStudent);
+    // document.getElementById('add-button').addEventListener('click', addStudent);
 };
 
 const init = () => {
